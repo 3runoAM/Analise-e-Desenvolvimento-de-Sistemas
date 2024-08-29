94cc28e00888bfc0140f9b6542d42945c8052050
Bruno Martins <jbrunno17@gmail.com>
2024-08-28 21:16:49 -0300

    Adiciona estrutura do 2 tp de html

--- Testes de performance por disciplina/Programação Web com JavaScript I (P1)/2º Teste de Performance/parte_4/questao_15/script.js
+++ Testes de performance por disciplina/Programação Web com JavaScript I (P1)/2º Teste de Performance/parte_4/questao_15/script.js
@@ -1,13 +1,13 @@
 const choices = ["Pedra", "Papel", "Tesoura"]
 
 let userChoice = Number(prompt("Escolha uma opção:\n1- Pedra\n2- Papel\n3- Tesoura"));
-let randomChoice = Math.floor(Math.random() * 3) + 1;
+let randomChoice = Math.floor(Math.random() * 3) +1;
 
 const userWins = "Você ganhou!"
 const userLoses = "Você perdeu!"
 const tie = "Empate!"
 
-alert(`${rockPaperScissors(userChoice, randomChoice)}\nVocê escolheu ${choices[userChoice - 1]} e o programa escolheu ${choices[randomChoice - 1]}`);
+alert(`${rockPaperScissors(userChoice, randomChoice)}\nVocê escolheu ${choices[userChoice-1]} e o programa escolheu ${choices[randomChoice-1]}`);
 
 function rockPaperScissors(userChoice, randomChoice) {
     if(isNaN(userChoice) || userChoice <= 0 || userChoice > 3) {
